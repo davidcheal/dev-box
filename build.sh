e801caa7f47fadbcac224ff4db650546cfbf9b6f
David Cheal <david.cheal@thermite.au>
2023-04-21 16:31:08 +1000

    WIP

--- build.sh
+++ build.sh
@@ -11,16 +11,25 @@ printer () {
  printf "${!level}$2${RESETCOLOR}\n"
 }
 
+cd ~/Downloads
 # Update Ubunutu and install apt software
-sudo apt-get update
 printer INFO "Running Ubuntu upgrade"
+sudo apt-get update
 sudo apt-get upgrade -y
+sudo do-release-upgrade -y
 printer INFO "Installing apt applications"
-sudo apt-get install net-tools php-fpm git nmap curl rar p7zip-full p7zip-rar vlc ffpmeg terminator libfuse2 -y
+sudo apt-get install net-tools php-fpm git nmap curl rar p7zip-full p7zip-rar vlc ffpmeg terminator libfuse2 \
+    open-vm-tools-desktop open-vm-tools openvpn \
+    -y
 printer INFO "Removing guff"
 sudo apt remove unattended-upgrades -y
 sudo apt-get autoremove -y
 # Install apps
+## AWS CLI
+printer INFO "Installing AWS CLI"
+$ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 ## VSCode
 printer INFO "Installing VSCode"
 sudo snap install --classic code
@@ -31,6 +40,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
 nvm install --lts
+## Global NPM Packages
+npm install -g npm-check-updates create-react-app express-generator
 ## Docker
 printer INFO "Installing Docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -40,3 +51,11 @@ sudo apt install docker-ce
 ## Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
+## Postman
+wget https://dl.pstmn.io/download/latest/linux_64
+tar -xf postman-linux-x64.tar.gz
+# Copy Config
+## Terminator
+cp terminator-config /home/dev/.config/terminator/config
+cp profile ~/.profile
+echo "export PS1='\[\033[1;32m\]$(whoami)@\[\033[1;34m\]$(hostname):\[\033[33m\]$(pwd)\[\033[0;37m\]\[\e[91m\]$(parse_git_branch)\[\e[00m\]\n'" >> ~/.bashrc

36b26f71500d46bd94e80eb29d6d05421fc6345f
David Cheal <david.cheal@thermite.au>
2023-04-22 13:04:44 +1000

    WIP

--- build.sh
+++ build.sh
@@ -25,30 +25,39 @@ else
 fi
 if [ ! -f  phase2 ]; then
     printer INFO "Doing release upgrade"
-    sudo do-release-upgrade 
+    sudo do-release-upgrade
     # -f DistUpgradeViewNonInteractive
     touch phase2
 else
     printer INFO "Skipping release upgrade"
 fi
 if [ ! -f  phase3 ]; then
+    mkdir ~/apps
     printer INFO "Installing apt applications"
-    sudo apt-get install net-tools php-fpm git nmap curl rar p7zip-full p7zip-rar vlc terminator libfuse2 \
+    APPS=~/apps
+    sudo apt-get install net-tools php-fpm git nmap curl rar \
+        p7zip-full p7zip-rar vlc terminator libfuse2 \
         open-vm-tools-desktop open-vm-tools openvpn \
+        kompare krusader trash-cli krename qbittorent \
+        filezilla libreoffice-calc
         -y
     sudo snap install ffmpeg
     sudo apt remove unattended-upgrades -y
     sudo apt-get autoremove -y
     # Install apps
+    ## KDENlive video editor
+    printer INFO "Installing KDENlive"
+    wget -O $APPS/kdenlive https://download.kde.org/stable/kdenlive/22.12/linux/kdenlive-22.12.3-x86_64.AppImage
+    chmod +x $APPS/kdenlive
     ## AWS CLI
     printer INFO "Installing AWS CLI"
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
-    sudo ./aws/install
+    wget -O temp/awscliv2.zip "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+    unzip awscliv2.zip temp/
+    sudo ./temp/aws/install
     ## VSCode
     printer INFO "Installing VSCode"
     sudo snap install --classic code
-    ## Node Version Manager
+    ## Node Version Manager, npm and Node
     printer INFO "Install Node Version Manager and NPM LTS"
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
@@ -64,16 +73,24 @@ if [ ! -f  phase3 ]; then
     sudo apt-cache policy docker-ce
     sudo apt install docker-ce -y
     ## Chrome
-    wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg --install google-chrome-stable_current_amd64.deb
+    wget -O temp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg --install temp/google-chrome-stable_current_amd64.deb
     ## Postman
-    wget -O postman-linux-x64.tar.gz https://dl.pstmn.io/download/latest/linux_64
-    tar -xf postman-linux-x64.tar.gz
+    wget -O temp/postman-linux-x64.tar.gz https://dl.pstmn.io/download/latest/linux_64
+    mkdir $APPS/postman
+    tar -xf temp/postman-linux-x64.tar.gz -C ~/apps/postman
+    ## Tor
+    wget -O temp/tor-browser-linux64-12.0.5_ALL.tar.xz https://www.torproject.org/dist/torbrowser/12.0.5/tor-browser-linux64-12.0.5_ALL.tar.xz
+    tar -xf temp/tor-browser-linux64-12.0.5_ALL.tar.xz -C ~/apps/
     # Copy Config
     ## Terminator
+    mkdir ~/.config/terminator
     cp terminator-config ~/.config/terminator/config
     cp profile ~/.profile
+    ## Set shell prompt
     echo "export PS1='\[\033[1;32m\]$(whoami)@\[\033[1;34m\]$(hostname):\[\033[33m\]$(pwd)\[\033[0;37m\]\[\e[91m\]$(parse_git_branch)\[\e[00m\]\n'" >> ~/.bashrc
+    # Clean up
+    rm temp/* -r
     touch phase3
 else
     printer INFO "Skipping apt applications"

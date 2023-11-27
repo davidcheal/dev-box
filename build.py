29e3df5deb7c02c2ee9f6e1947ec259679579338
David Cheal <david.cheal@gmail.com>
2023-11-26 22:06:41 -0800

    Working on python use

--- build.py
+++ build.py
@@ -23,72 +23,67 @@ VMWARE = False
 LINUX_APT = "sudo apt-get install -y"
 MACOS_BREW = "brew install -y",
 
-BASE_LINUX_APPS = [
-    {"name":"Curl", "package_name":"curl", "options":None},
-    {"name":"DMI Decode", "package_name":"dmidecode", "options":None},
-    {"name":"Firefox", "package_name":"firefox", "options":None},
-    {"name":"GPG", "package_name":"gpg", "options":None},
-    {"name":"jq", "package_name":"jq", "options":None},
-    {"name":"Kompare", "package_name":"kompare", "options":None},
-    {"name":"KRename", "package_name":"krename", "options":None},
-    {"name":"Krusader", "package_name":"krusader", "options":None},
-    {"name":"librefuse2", "package_name":"librefuse2", "options":None},
-    {"name":"jq", "package_name":"jq", "options":None},
-    {"name":"libreoffice-calc", "package_name":"libreoffice-calc", "options":None},
-    {"name":"Network Tools", "package_name":"net-tools", "options":None},
-   
-    # "libreoffice-calc",
-    # "net-tools",
-    # "p7zip-full",
-    # "p7zip-rar",
-    # "python3-pip",
-    # "qbittorrent",
-    # "rar",
-    # "terminator",
-    # "trash-cli",
-    # "vlc",
+LINUX_COMMANDS = [
+    "sudo apt-get update",
+    "wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o microsoft.asc.gpg",
+    "sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/",
+    "wget https://packages.microsoft.com/config/ubuntu/20.04/prod.list",
+    "sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list",
+    "sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg",
+    "sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list",
+    "sudo apt-get remove thunderbird -y",
+    "sudo apt-get remove --purge libreoffice* -y",
+    "sudo apt remove unattended-upgrades -y",
+    "sudo apt-get autoremove -y",
+    "sudo apt-get upgrade -y"
 ]
 
-CROSS_PLATFORM = [
-    {
-        "name": "ffmpeg",
-        "package_name": "ffmpeg",
-        
-    },
-    {
-        "name": "aws cli",
-        "package_name": "aws",
-        "linux": "sudo apt-get install -y",
-        "macos": "brew install -y",
-    }
+def linux_commands(LINUX_COMMANDS):
+    try:
+        for COMMAND in LINUX_COMMANDS:
+            subprocess.check_call(COMMAND, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
+            printer(SUCCESS, COMMAND + " successful")
+    except subprocess.CalledProcessError:
+        printer(CRIT, COMMAND + " failed")
+        sys.exit(1)
+
+LINUX_BASE_APPS = [
+    {"name":"Curl", "package_name":"curl", "options":None, "installer":"apt"},
+    {"name":"DMI Decode", "package_name":"dmidecode", "options":None,"installer":"apt"},
+    {"name":"Firefox", "package_name":"firefox", "options":None,"installer":"apt"},
+    {"name":"GPG", "package_name":"gpg", "options":None,"installer":"apt"},
+    {"name":"JQ", "package_name":"jq", "options":None,"installer":"apt"},
+    {"name":"Kompare", "package_name":"kompare", "options":None,"installer":"apt"},
+    {"name":"K Rename", "package_name":"krename", "options":None,"installer":"apt"},
+    {"name":"Krusader", "package_name":"krusader", "options":None,"installer":"apt"},
+    {"name":"Libre Office Calc", "package_name":"libreoffice-calc", "options":None,"installer":"apt"},
+    {"name":"Network Tools", "package_name":"net-tools", "options":None,"installer":"apt"},
+    {"name":"P7 Zip", "package_name":"p7zip-full", "options":None,"installer":"apt"},
+    {"name":"P7 RAR", "package_name":"p7zip-rar", "options":None,"installer":"apt"},
+    {"name":"Python 3 PIP", "package_name":"python3-pip", "options":None,"installer":"apt"},
+    {"name":"Qbittorrent", "package_name":"qbittorrent", "options":None,"installer":"apt"},
+    {"name":"Rar", "package_name":"rar", "options":None,"installer":"apt"},
+    {"name":"Terminator", "package_name":"terminator", "options":None,"installer":"apt"},
+    {"name":"Trash CLI", "package_name":"trash-cli", "options":None,"installer":"apt"},
+    {"name":"VLC Player", "package_name":"vlc", "options":None,"installer":"apt"},
+    {"name":"FF Mpeg", "package_name":"ffmpeg", "options":None,"installer":"apt"}
 ]
 
-DEV_LINUX_APP = [
-    "aspnetcore-runtime-7.0",
-    "default-jdk",
-    "default-jre",
-    "dotnet-sdk-7.0",
-    "filezilla",
-    "golang-go",
-    "nginx",
-    "nmap",
-    "openvpn",
-    "php-fpm",
-    "ruby-full",
-]
-
-BREW_APPS = [
-    "curl",
-    "php",
-    "vlc",
-    "ruby",
-    "python",
-    "postman",
-    "openvpn-connect",
-    "zip",
-    "sevenzip",
-    "rar" "wget" "nginx" "xtorrent" "java",
-    "go",
+LINUX_DEV_APPS = [
+    {"name":".Net Core Runtime", "package_name":"aspnetcore-runtime-7.0", "options":None,"installer":"apt"},
+    {"name":".Net Core SDK", "package_name":"dotnet-sdk-7.0", "options":None,"installer":"apt"},
+    {"name":"Java JDK", "package_name":"default-jdk", "options":None,"installer":"apt"},
+    {"name":"Java JRE", "package_name":"default-jre", "options":None,"installer":"apt"},
+    {"name":"FileZilla", "package_name":"filezilla", "options":None,"installer":"apt"},
+    {"name":"GoLang", "package_name":"golang-go", "options":None,"installer":"apt"},
+    {"name":"GoLang", "package_name":"golang-go", "options":None,"installer":"apt"},
+    {"name":"NGINX", "package_name":"nginx", "options":None,"installer":"apt"},
+    {"name":"NMap", "package_name":"nmap", "options":None,"installer":"apt"},
+    {"name":"Open VPN", "package_name":"Open VPN", "options":None,"installer":"apt"},
+    {"name":"PHP FPM", "package_name":"php-fpm", "options":None,"installer":"apt"},
+    {"name":"Ruby", "package_name":"ruby-full", "options":None,"installer":"apt"},
+    {"name":"PyCharm Professional", "package_name":"pycharm-professional --classic", "options":None},
+    {"name":"Postman", "package_name":"postman", "options":None}
 ]
 
 def printer(color, text):
@@ -96,12 +91,10 @@ def printer(color, text):
 
 
 # Detect VMWARE
-DMI_CODE = subprocess.check_call("sudo dmidecode -s system-manufacturer", shell=True)
-if re.match("VMware", DMI_CODE):
+DMI_CODE = subprocess.check_output("sudo dmidecode -s system-manufacturer", shell=True)
+
+if re.match("VMware", str(DMI_CODE,encoding='utf-8')):
     VMWARE = True
-
-
-
 # Detect Ubuntu or MacOs
 if "linux" in platform.system().lower():
     OS = "linux"
@@ -117,68 +110,27 @@ else:
 
 def install_linux_packages(packages):
     try:
-        APPS = " ".join(map(str, packages))
-        for APP in APPS:
-            INSTALLED = True if subprocess.call("which " + APP.package_name, shell=True) else False
+        for APP in packages:
+            INSTALLED = False
+            if subprocess.call("which " + APP["package_name"], shell=True) == 0:
+                INSTALLED = True
             if not INSTALLED:
-                subprocess.call(APP[OS] + " " + APP.package_name, shell=True)
-                # subprocess.check_call("sudo apt-get install -y " + apps, shell=True)
-                printer(SUCCESS, f"{packages} installation successful")
+                printer(INFO, "Installing "+APP["name"])
+                subprocess.check_call("sudo apt-get install -y " + APP["package_name"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
+                printer(SUCCESS, APP["name"] + " installation successful")
+            else:
+                printer(INFO, APP["name"] + "already installed")
     except subprocess.CalledProcessError:
-        printer(CRIT, f"{packages} installation failed")
-        sys.exit(1)
-
-def install_python_package(package_name):
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
-        print(f"{SUCCESS}{package_name} installation successful.")
-    except subprocess.CalledProcessError:
-        print(f"{CRIT}Error occurred during {package_name} installation.")
+        printer(CRIT, APP["name"] + " installation failed")
         sys.exit(1)
 
 BOX_TYPE = input(f"What box type?: minimal or [{BOX_TYPE}] ") or BOX_TYPE
 EMAIL = input(f"What is your email address?: [{EMAIL}]") or EMAIL
 NAME = input(f"What is your name?: [{NAME}]") or NAME
 
-# Install OS specific apps
-if OS == "linux":
-    COMMANDS = [
-        "sudo apt-get update",
-        "wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o microsoft.asc.gpg",
-        "sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/",
-        "wget https://packages.microsoft.com/config/ubuntu/20.04/prod.list",
-        "sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list",
-        "sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg",
-        "sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list",
-        "sudo apt-get remove thunderbird -y",
-        "sudo apt-get remove --purge libreoffice* -y",
-        "sudo apt remove unattended-upgrades -y",
-        "sudo apt-get autoremove -y",
-        "sudo apt-get upgrade -y",
-    ]
-    # Install vmware tools on VM Guests
-    if VMWARE:
-        subprocess.check_call(
-            "sudo apt-get install open-vm-tools-desktop open-vm-tools -y", shell=True
-        )
-    # Make project folders
-    subprocess.check_call("sudo mkdir -p " + LINUX_APP_FOLDER, shell=True)
-    subprocess.check_call("sudo chown -R $USER:$USER " + LINUX_APP_FOLDER, shell=True)
-    # Snaps
-    subprocess.check_call("sudo snap install ffmpeg", shell=True)
-    subprocess.check_call("sudo snap install postman", shell=True)
-if OS == "macos":
-    subprocess.check_call(
-        'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"',
-        shell=True,
-    )
-    subprocess.check_call("brew update", shell=True)
-
-    subprocess.check_call("brew install " + " ".join(map(str, BREW_APPS)), shell=True)
-    subprocess.check_call("brew install --cask firefox", shell=True)
 
 
-for app in CROSS_PLATFORM:
-    INSTALLED = True if subprocess.call("which " + app.package_name, shell=True) else False
-    if not INSTALLED:
-        subprocess.call(app[OS] + " " + app.package_name, shell=True)
+linux_commands(LINUX_COMMANDS)
+install_linux_packages(LINUX_BASE_APPS)
+install_linux_packages(LINUX_DEV_APPS)
+

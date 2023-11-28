a84e21678e4c857f7b6a7acc525f197a702db08d
David Cheal <david.cheal@gmail.com>
2023-11-27 16:25:11 -0800

    WIP

--- build.py
+++ build.py
@@ -52,7 +52,7 @@ LINUX_COMMANDS = [
     "sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list",
     "sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg",
     "sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list",
-    "wget -q -O- https://packages.openvpn.net/packages-repo.gpg | sudo tee /etc/apt/keyrings/openvpn.asc",
+    "wget https://packages.openvpn.net/packages-repo.gpg -O /etc/apt/keyrings/openvpn.asc",
     "echo deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian jammy main | sudo tee /etc/apt/sources.list.d/openvpn-packages.list",
     "sudo apt-get update",
     "sudo apt-get remove thunderbird -y",
